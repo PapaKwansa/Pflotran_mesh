f3fca91560b370e691aac44051e16f0268da8135
Henry Arhin <arhin2014@gmail.com>
2026-04-18 00:28:46 -0400

    Changed the PFLOTRAN execution path in batch script

--- avant_mesh/job.sh
+++ avant_mesh/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pflotran
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
+#SBATCH --ntasks=60
 #SBATCH --mem=250G
 #SBATCH --time=48:00:00
 #SBATCH --output=pflotran_%j.out
@@ -10,4 +10,7 @@
 set -euo pipefail
 
 cd /home/harhin/Pflotran_mesh/avant_mesh
-srun -n 64 /home/harhin/PFLOTRAN/petsc/pflotran/src/pflotran/pflotran -input_prefix layers4
+
+/home/harhin/PFLOTRAN/petsc/arch-linux-c-opt/bin/mpirun -np 60 \
+  /home/harhin/PFLOTRAN/petsc/pflotran/src/pflotran/pflotran \
+  -input_prefix layers4
