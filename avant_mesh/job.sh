b1a7ed4e8976746a2064dc39cf2bac783d226fa1
Henry Arhin <arhin2014@gmail.com>
2026-04-18 00:13:59 -0400

    Renamed the batch file

---
+++ avant_mesh/job.sh
@@ -0,0 +1,15 @@
+cat > job.sh <<'EOF'
+#!/bin/bash
+#SBATCH --job-name=pflotran
+#SBATCH --nodes=1
+#SBATCH --ntasks=64
+#SBATCH --mem=250G
+#SBATCH --time=48:00:00
+#SBATCH --output=pflotran_%j.out
+#SBATCH --error=pflotran_%j.err
+
+set -euo pipefail
+
+cd /home/harhin/Pflotran_mesh/avant_mesh
+srun -n 64 /home/harhin/PFLOTRAN/petsc/pflotran/src/pflotran/pflotran -input_prefix layers4
+EOF
