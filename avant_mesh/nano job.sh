417cba8c5c91e8f0ab007408b5adf2674d8c2285
Henry Arhin <arhin2014@gmail.com>
2026-04-17 23:49:53 -0400

    Changed the batch script

--- avant_mesh/nano job.sh
+++ avant_mesh/nano job.sh
@@ -6,17 +6,10 @@
 #SBATCH --time=48:00:00
 #SBATCH --output=pflotran_%j.out
 #SBATCH --error=pflotran_%j.err
-# If Palmetto requires them, uncomment and fill these in:
-##SBATCH --account=YOUR_ACCOUNT
-##SBATCH --partition=YOUR_PARTITION
 
 set -euo pipefail
-
-module load pflotran
 
 cd /home/harhin/Pflotran_mesh/avant_mesh
 git pull
 
-srun -n "$SLURM_NTASKS" /home/harhin/PFLOTRAN/petsc/pflotran/src/pflotran/pflotran -input_prefix layers4
-
-python postprocess.py
+srun -n 64 /home/harhin/PFLOTRAN/petsc/pflotran/src/pflotran/pflotran -input_prefix layers4
