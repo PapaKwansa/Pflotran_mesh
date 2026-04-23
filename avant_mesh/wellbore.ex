4bedabc153057d5d64670286b7cb9362e88d3a3a
Henry Arhin <hubblescope50@gmail.com>
2026-04-23 13:51:18 -0400

    Changed the injection zone to use .vset file

--- avant_mesh/wellbore.ex
+++ avant_mesh/wellbore.ex
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:472dbc5dfc478f89ace4cad2959e2e9a18ba77b2e629a5e77df89e7768bde75a
-size 678
+oid sha256:f16bc2e2f47dda1bef2ea2dad7c422ccc6a710109df1695cd18a2bf8959d2ca3
+size 2671
